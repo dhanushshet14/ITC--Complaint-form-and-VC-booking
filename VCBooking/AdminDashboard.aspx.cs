005f8fc6fe67afe8c96b7ed3afa0953950e0e37b
Bharath B Shetty <158118484+gigfrggew@users.noreply.github.com>
2026-03-04 17:55:23 +0530

    Updated VCBooking project and excluded Login & SignUp pages (#1)
    * Add .gitattributes and .gitignore.
    
    * Add project files.
    
    * Completed VC booking system before moving to group repo
    
    * Post-merge project file update
    
    * Removed TMG_ITC project from main-bharath branch
    
    * Updated project structure from WebFormDemo to VCBooking
    
    * Ignored Login and SignUp page

---
+++ VCBooking/AdminDashboard.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace VCBooking
+{
+    public partial class AdminDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadMeetings();
+            }
+        }
+
+        private void LoadMeetings()
+        {
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["HRConnection"].ConnectionString))
+            {
+                string query = @"SELECT VCId, Topic, VCDate, VCStatus, CreatedBy
+                 FROM VCRequestHeader
+                 WHERE VCStatus <> 'Cancelled'
+                 ORDER BY CreatedDate DESC";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvMeetings.DataSource = dt;
+                gvMeetings.DataBind();
+            }
+        }
+
+        private void SoftDeleteMeeting(string vcId, string reason)
+        {
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["HRConnection"].ConnectionString))
+            {
+                string query = @"UPDATE VCRequestHeader
+                                 SET VCStatus = 'Cancelled',
+                                     CancelledBy = @CancelledBy,
+                                     CancelledDate = GETDATE(),
+                                     CancelReason = @CancelReason
+                                 WHERE VCId = @VCId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@VCId", vcId);
+
+                string cancelledBy = Session["Username"] != null
+                                     ? Session["Username"].ToString()
+                                     : "Admin";
+
+                cmd.Parameters.AddWithValue("@CancelledBy", cancelledBy);
+                cmd.Parameters.AddWithValue("@CancelReason", reason);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+        protected void gvMeetings_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteMeeting")
+            {
+                hfVCId.Value = e.CommandArgument.ToString();
+                pnlDelete.Visible = true;
+            }
+        }
+
+
+        protected void btnConfirmDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtReason.Text))
+            {
+                return; // Later we can show validation message
+            }
+
+            SoftDeleteMeeting(hfVCId.Value, txtReason.Text);
+
+            pnlDelete.Visible = false;
+            txtReason.Text = "";
+
+            LoadMeetings(); // Refresh grid
+        }
+
+        protected void btnCancelPopup_Click(object sender, EventArgs e)
+        {
+            pnlDelete.Visible = false;
+            txtReason.Text = "";
+        }
+    }
+}
---
+++ VCBooking/ViewRequests.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewRequests.aspx.cs" Inherits="VCBooking.ViewRequests" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <style>
+        .table thead th {
+            background-color: dodgerblue !important;
+            color: white !important;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="gvRequests"
+                runat="server"
+                CssClass="table table-bordered table-striped"
+                AutoGenerateColumns="false">
+                <Columns>
+
+                    <asp:BoundField DataField="VCId" HeaderText="VC ID" ><HeaderStyle CssClass="bg-primary text-white" /> </asp:BoundField>
+
+                    <asp:BoundField DataField="CompanyName" HeaderText="Company" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="VCTypeName" HeaderText="VC Type" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="VCAccountName" HeaderText="VC Account" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="Topic" HeaderText="Topic" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="VCDate"
+                        HeaderText="Date"
+                        DataFormatString="{0:dd-MMM-yyyy}" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="FromTime"
+                        HeaderText="From"
+                        DataFormatString="{0:hh:mm tt}" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="ToTime"
+                        HeaderText="To"
+                        DataFormatString="{0:hh:mm tt}" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="LocationName" HeaderText="Location" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                    <asp:BoundField DataField="VCStatus" HeaderText="Status" ><HeaderStyle CssClass="bg-primary text-white" /></asp:BoundField>
+
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
